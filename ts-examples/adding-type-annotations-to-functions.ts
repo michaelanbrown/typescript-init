b2af3da60671774c983abda2d245b6b3ccb5daec
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-29 10:16:35 -0500

    Adding a folder to store typeScript examples and typeScript example of adding type annotations to functions

---
+++ ts-examples/adding-type-annotations-to-functions.ts
@@ -0,0 +1,10 @@
+// JavaScript:
+
+// function dullFunc(value1, value2) {
+    // return "I'm boring and difficult to read."
+// }
+
+//TypeScript:
+function funFunc(score: number, message: string): string {
+    return "I've got personality and I'm helpful!"
+}
