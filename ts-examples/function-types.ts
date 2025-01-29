5ca88655b397f04c44309cf5207955492a42c6fa
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-29 11:49:05 -0500

    Adding examples and notes for function types

---
+++ ts-examples/function-types.ts
@@ -0,0 +1,14 @@
+// The string and number parameters define the type of the function
+function CreateMovieID(name: string, id: number): string {
+    return name + id;
+}
+
+//Want to assign this function to a variable and use the variable to call it
+let x: number;
+x = 5;
+
+let IdGenerator: (chars: string, nums: number) => string;
+IdGenerator = (name: string, id: number) => name + id;
+
+let newID: string = IdGenerator('jedi', 20);
+console.log(newID); //jedi20
