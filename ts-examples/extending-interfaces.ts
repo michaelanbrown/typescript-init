f7e14810997d9d93b45f5f7ed6912403a8a404ee
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-30 12:12:42 -0500

    Notes examples of extening interfaces

---
+++ ts-examples/extending-interfaces.ts
@@ -0,0 +1,20 @@
+interface LibraryResource {
+    catalogNumber: number;
+}
+
+interface Book {
+    title: string;
+}
+
+// Has properties defined on previous interfaces, and its own
+interface Encyclopedia extends LibraryResource, Book {
+    volume: number;
+}
+
+//Declaring a variable with the Encyclopedia type must have all three properties because none are optional
+let refBook: Encyclopedia = {
+    catalogNumber: 1234,
+    title: 'The Book of Everything',
+    volume: 1
+}
+
