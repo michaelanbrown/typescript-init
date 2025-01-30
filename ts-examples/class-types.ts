1f9d2ad4c74979d55f6187f183101f1da3968950
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-30 12:28:37 -0500

    Added notes and a demo for class typing and interfaces

--- ts-examples/class-types.ts
+++ ts-examples/class-types.ts
@@ -11,3 +11,32 @@ class ElementarySchoolLibrarian implements Librarian {
 //This means that the object assigned to it must implement the Librarian interface
 let kidsLibrarian: Librarian = new ElementarySchoolLibrarian();
 kidsLibrarian.doWork();
+
+//Demo:
+interface Person {
+    name: string;
+    email: string;
+}
+
+interface Director extends Person {
+    numMoviesDirected: number;
+}
+
+interface CastMember extends Person {
+    role: string;
+    rehearse: (sceneNumber: number) => void;
+}
+
+class Performer implements CastMember {
+    name: string = "";
+    email: string = "";
+    role: string = "";
+
+    rehearse(sceneNumber: number): void {
+        console.log(`${this.name} is rehearsing scene number ${sceneNumber}.`)
+    }
+}
+
+let favoriteCastMember: CastMember = new Performer();
+favoriteCastMember.name = 'Daisy';
+favoriteCastMember.rehearse(25);
