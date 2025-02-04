aa5d9f89f6a223e3bdcd1ea4c138b8533886a6c5
michaelanbrown <m_brownbrown@yahoo.com>
2025-02-04 08:53:46 -0500

    Refactoring the asynchronous example to use async and await

--- ts-examples/asynchronous-example.ts
+++ ts-examples/asynchronous-example.ts
@@ -1,3 +1,30 @@
+// function getMoviesByDirector(director: string): Promise<string[]> {
+//     let  p: Promise<string[]> = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let foundMovies: string[] = GetTitles(director);
+
+//             if(foundMovies.length > 0) {
+//                 resolve(foundMovies);
+//             } else {
+//                 reject('No movies found for that director,');
+//             }
+//         }, 2000);
+//     });
+//     return p;
+// }
+
+// console.log('Beginning search...'); //Performs 1st
+// getMoviesByDirector('George Lucas') //Performs 3rd
+//     .then(titles => {
+//         console.log(`Found titles: ${titles}`);
+//         return titles.length; //this is the resolve value therefor can automatically be used in the next then
+//     }, reason => {return 0; }) //passes to the next call to then below before it gets to the catch function. Now the catch function is only needed to catch any errors
+//     .then(numOfMovies => console.log(`Number of movies: ${numOfMovies}`))
+//     .catch(reason => console.log(`Error: ${reason}`));
+// console.log('Search submitted...'); //Performs 2nd
+
+//--------REFACTOR TO USE ASYNC AND AWAIT------------
+
 function getMoviesByDirector(director: string): Promise<string[]> {
     let  p: Promise<string[]> = new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -13,12 +40,12 @@ function getMoviesByDirector(director: string): Promise<string[]> {
     return p;
 }
 
+async function logSearchResults(director: string) {
+    let foundMovies = await getMoviesByDirector(director);
+    console.log(foundMovies);
+}
+
 console.log('Beginning search...'); //Performs 1st
-getMoviesByDirector('George Lucas') //Performs 3rd
-    .then(titles => {
-        console.log(`Found titles: ${titles}`);
-        return titles.length; //this is the resolve value therefor can automatically be used in the next then
-    }, reason => {return 0; }) //passes to the next call to then below before it gets to the catch function. Now the catch function is only needed to catch any errors
-    .then(numOfMovies => console.log(`Number of movies: ${numOfMovies}`))
-    .catch(reason => console.log(`Error: ${reason}`));
+logSearchResults('George Lucas'); //Performs 3rd
+    .catch(reason => console.log(reason));
 console.log('Search submitted...'); //Performs 2nd
