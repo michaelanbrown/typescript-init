69f1916363703f4f0df4d39bc774f362e1a6cba1
michaelanbrown <m_brownbrown@yahoo.com>
2025-02-11 09:13:49 -0500

    Installing type declaration files demo steps

---
+++ ts-examples/installing-and-using-a-type-declaration-file.ts
@@ -0,0 +1,7 @@
+//Search for package(s) at npmjs.com
+//Example for lodash:
+    // 1. npm install lodash
+    // 2. npm install @types/lodash
+        // May have different version numbers
+
+import * as _ from "lodash";
